842271356c2cfa80d737ade089786649a10921de
Sentientprism <179720166+4RVCC30@users.noreply.github.com>
2025-12-11 11:59:47 -0500

    fucked some shit up
    added a deck makers and hands

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,195 @@
+using UnityEngine;
+using Unity.Netcode;
+using System;
+using NUnit.Framework;
+public class GameManager : NetworkBehaviour
+{
+    public NetworkList<Card> NetworkDeck;
+
+    public NetworkList<Card> NetworkDiscard;
+
+    [SerializeField]
+    public Card[,] Hands = new Card[4,6];
+
+    // Establishes a Singleton
+    public static GameManager instance;
+
+    // Could we do this in the OnNetworkSpawn()
+    private void Awake()
+    {
+        if (instance != null && instance != this)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        NetworkDeck = new NetworkList<Card>();
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+
+    //Makes a new NetworkVarible of type Card (see def) called randomValues and then sets the read perms to everyone and the writing perms to only the server
+    //See this link for what can be NetworkVariables https://docs.unity3d.com/Packages/com.unity.netcode.gameobjects@2.7/manual/serialization.html
+    public NetworkVariable<Card> cardValue = new NetworkVariable<Card>(
+        new Card
+        {
+            //sets default values
+            colorOne = 0,
+            colorTwo = 1,
+            ColorVisibleToOwner = 0,
+        },
+        //sets var perms
+        NetworkVariableReadPermission.Everyone, NetworkVariableWritePermission.Server);
+
+    //CardDeck Initialization
+
+
+    //creates a new Card struct that extends INetworkSerializable
+
+    //We make a struct instead of a Class because a class is a reference type i ~think~ either way if we're storing a bunch of values this is how
+    public struct Card : INetworkSerializable, IEquatable<Card>
+    {
+        //0 Blue,1 Purple,2 Green, 3 Yellow, 4 Red
+        public int colorOne;
+        public int colorTwo;
+        //Between 0-1 (0 for side One, and 1 for side Two)
+        public int ColorVisibleToOwner;
+
+        //idk why we do this but just make sure you do this whenever you make a struct (we can figure out why later)
+        public void NetworkSerialize<T>(BufferSerializer<T> serializer) where T : IReaderWriter
+        {
+            serializer.SerializeValue(ref colorOne);
+            serializer.SerializeValue(ref colorTwo);
+            serializer.SerializeValue(ref ColorVisibleToOwner);
+        }
+        public bool Equals(Card other)
+        {
+            return colorOne == other.colorOne &&
+                   colorTwo == other.colorTwo &&
+                   ColorVisibleToOwner == other.ColorVisibleToOwner;
+        }
+    }
+    //0 Blue,1 Purple,2 Green, 3 Yellow, 4 Red
+    public Card[] deckCardRange = new Card[15]
+    {
+        new() {colorOne = 4, colorTwo = 4, ColorVisibleToOwner = 0},
+        new() {colorOne = 4, colorTwo = 2, ColorVisibleToOwner = 0},
+        new() {colorOne = 4, colorTwo = 0, ColorVisibleToOwner = 0},
+        new() {colorOne = 4, colorTwo = 1, ColorVisibleToOwner = 0},
+        new() {colorOne = 4, colorTwo = 3, ColorVisibleToOwner = 0},
+
+        new() {colorOne = 2, colorTwo = 2, ColorVisibleToOwner = 0},
+        new() {colorOne = 2, colorTwo = 0, ColorVisibleToOwner = 0},
+        new() {colorOne = 2, colorTwo = 1, ColorVisibleToOwner = 0},
+        new() {colorOne = 2, colorTwo = 3, ColorVisibleToOwner = 0},
+
+        new() {colorOne = 0, colorTwo = 0, ColorVisibleToOwner = 0},
+        new() {colorOne = 0, colorTwo = 1, ColorVisibleToOwner = 0},
+        new() {colorOne = 0, colorTwo = 3, ColorVisibleToOwner = 0},
+
+        new() {colorOne = 1, colorTwo = 1, ColorVisibleToOwner = 0},
+        new() {colorOne = 1, colorTwo = 3, ColorVisibleToOwner = 0},
+
+        new() {colorOne = 3, colorTwo = 3, ColorVisibleToOwner = 0}
+    };
+
+
+    //Overrides the OnNetworkSpawn and adds debug log changes to the OnValueChanged method
+    public override void OnNetworkSpawn()
+    {
+        if (IsServer)
+        {
+            creatDeck();
+            dealOut();
+        }
+        //uses a Lamda operator to add further methods to the OnValueChanged method
+        //(I tried to edit it to prevent values from being changed outside their limits, edited them out cause i couldnt figure it out)
+        cardValue.OnValueChanged += (Card previousValue, Card newValue) =>
+        {
+            /*
+             * I tried placing a clamping method in this code so the server has stricter control on the changing of
+             * values. shelved for now for sake of simplicity
+             * 
+             * if (newValue.colorOne > 3 || newValue.colorOne < 0)
+             {
+                 newValue.colorOne = previousValue.colorOne;
+             }
+             if (newValue.colorTwo > 3 || newValue.colorTwo < 0)
+             {
+                 newValue.colorTwo = previousValue.colorTwo;
+             }
+             if (newValue.ColorVisibleToOwner > 1 || newValue.ColorVisibleToOwner < 0)
+             {
+                 newValue.ColorVisibleToOwner = previousValue.ColorVisibleToOwner;
+             }
+            */
+            Debug.Log(OwnerClientId + "; " + newValue.colorOne + " ; " + newValue.colorTwo + "; " + newValue.ColorVisibleToOwner);
+        };
+    }
+    //create this bool to control the rate at which invoke is called 
+    public bool latch = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Only the server can do anything in Update else return;
+        if (!IsServer) return;
+    }
+    //Updates the NetWorkValue randomValues 
+    private void updateValues()
+    {
+
+    }
+
+
+    public void dealOut()
+    {
+        for (int i = 0; i < Hands.GetLength(0); i++)
+        {
+            string debug = "";
+            for (int j = 0; j < Hands.GetLength(1); j++)
+            {
+                int rand = UnityEngine.Random.Range(0, NetworkDeck.Count);
+                Hands[i, j] = NetworkDeck[rand];
+                NetworkDiscard.Add(NetworkDeck[rand]);
+                NetworkDeck.Remove(NetworkDeck[rand]);
+
+                debug += " " + Hands[i,j].colorOne;
+            }
+
+            Debug.Log("Hand: " + i + " = { " + debug + " }");
+        }
+        GetHandsclientRpc(Hands);
+    }
+
+    [ClientRpc]
+    public void GetHandsclientRpc(Card[,] newHands)
+    {
+        Hands = newHands;
+    }
+
+    public void creatDeck()
+    {
+        if (!IsServer) return;
+
+        int index = 0;
+
+        Card[] deck = new Card[90];
+
+        for (int i = 0; i < deckCardRange.Length; i++)
+        {
+            for (int j = 0; j < 6; j++)
+            {
+                deck[i * 6 + j] = deckCardRange[index];
+                index++;
+                if (index == deckCardRange.Length) index = 0;
+            }
+        }
+
+        for (int i = 0; i < deck.Length; i++)
+        {
+            NetworkDeck.Add(deck[i]);
+        }
+    }
+
+}
---
+++ Assets/Scripts/UIJOINSCRIPT.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+using Unity.Networking;
+using Unity.Netcode;
+using Unity.Netcode.Transports.UTP;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using TMPro;
+
+public class UIJOINSCRIPT : NetworkBehaviour
+{
+
+    public TMP_InputField inputField;
+
+    
+
+    public void JoinHost()
+    {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = inputField.text;
+        NetworkManager.Singleton.StartHost();
+        if(!IsClient)NetworkManager.Singleton.SceneManager.LoadScene("CoreGame", LoadSceneMode.Single);
+    }
+    public void StartClient()
+    {
+        NetworkManager.Singleton.GetComponent<UnityTransport>().ConnectionData.Address = inputField.text;
+        NetworkManager.Singleton.StartClient();
+    }
+}
